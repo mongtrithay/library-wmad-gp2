6b95ab89e4c7c7220065f704982b920493a3415a
mongtrithay <trithay.mong@institute.pse.ngo>
2024-08-01 15:11:10 +0700

    update book catalog

--- src/pages/bookCatalog/BookCatalogPage.jsx
+++ src/pages/bookCatalog/BookCatalogPage.jsx
@@ -3,8 +3,9 @@ import React from "react";
 import { useState, useEffect } from "react";
 const BookCatalogPage = () => {
   const [books, setBooks] = useState([]);
-  const url = 'http://localhost:3000/api/books';
-  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjQsImlhdCI6MTcyMjQxMDIyMCwiZXhwIjoxNzIyNDQ2MjIwfQ.U775eAQT3Y33RkRrKP3mcMJf62WDpiWLEqTNoZo-C34';
+  const url = "http://localhost:3000/api/books";
+  const token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjQsImlhdCI6MTcyMjQ5ODA4MCwiZXhwIjoxNzIyNTM0MDgwfQ.08fmHUt0Oo10tPB-16ftZ_7Ff0KIZ1iKHb09b6h4U7k";
   const obj = {
     method: "GET",
     headers: {
@@ -14,45 +15,51 @@ const BookCatalogPage = () => {
   };
   useEffect(() => {
     const fetchData = async () => {
-      try{
+      try {
         const respone = await axios.get(url, obj);
-      const data = respone.data;
-      setBooks(data)
-      console.log(data);
-      }catch(error){
-        console.error('Data oun bc drink beer',error);
+        const data = respone.data;
+        setBooks(data);
+        console.log(data);
+      } catch (error) {
+        console.error("Data oun bc drink beer", error);
       }
-    }
+    };
     fetchData();
-    }, []);
+  }, []);
   return (
     <div className=" bg-gray-100">
       <h1 className=" my-4 text-3xl font-bold">Book Catalogs</h1>
-      <button class="bg-sky-500 my-4 text-white font-bold py-4 px-8 rounded-lg ">Create</button>
-      <table class="border-separate  my-4 text-left rounded-lg border-spacing-4 border-4 border-slate-400 ...">
+      <button class="bg-sky-500 my-4 text-white font-bold py-4 px-8 rounded-lg ">
+        Create
+      </button>
+      <table class=" my-4 text-left  border-4 border-slate-400 ...">
         <thead>
-          <tr class="border-b-2 border-gray-600">
-            <th class="">Action</th>
-            <th class="">ISBN</th>
-            <th class="">Title</th>
-            <th class="">Authors</th>
-            <th class="">Publisher</th>
-            <th class="">Genre</th>
-            <th class="">Shelf Location</th>
+          <tr class="px-6 py-6 border-b-2 border-gray-300">
+            <th class="px-5 py-5">Action</th>
+            <th class="px-5 py-5">ISBN</th>
+            <th class="px-5 py-5">Title</th>
+            <th class="px-5 py-5">Authors</th>
+            <th class="px-5 py-5">Publisher</th>
+            <th class="px-5 py-5">Genre</th>
+            <th class="px-5 py-5">Shelf Location</th>
           </tr>
         </thead>
         <tbody>
-            {books.map(data => (
-              <tr>
-              <td class="bg-sky-500 text-center py-1 px-8 text-white rounded-lg">view</td>
-              <td class="">{data.isbn}</td>
-              <td class="">{data.title}</td>
-              <td class="">{data.authors}</td>
-              <td class="">{data.publisher}</td>
-              <td class="">{data.genre}</td>
-              <td class="">{data.shelf_location}</td>
+          {books.map((data) => (
+            <tr className="border-b-2 border-gray-300">
+              <td class="px-5 py-5">
+                <button class="bg-sky-500 text-center py-1 px-4 text-white rounded-lg">
+                  view
+                </button>
+              </td>
+              <td class="px-5 py-5">{data.isbn}</td>
+              <td class="px-5 py-5">{data.title}</td>
+              <td class="px-5 py-5">{data.authors}</td>
+              <td class="px-5 py-5">{data.publisher}</td>
+              <td class="px-5 py-5">{data.genre}</td>
+              <td class="px-5 py-5">{data.shelf_location}</td>
             </tr>
-            ))}
+          ))}
         </tbody>
       </table>
     </div>
