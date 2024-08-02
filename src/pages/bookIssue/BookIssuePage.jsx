7bda4fecdcae8d68eaab31ac8f0aea2da0d3d006
mongtrithay <trithay.mong@institute.pse.ngo>
2024-08-02 15:39:03 +0700

    fetch data api bookIssu

--- src/pages/bookIssue/BookIssuePage.jsx
+++ src/pages/bookIssue/BookIssuePage.jsx
@@ -1,4 +1,30 @@
+import {useState, useEffect} from "react";
+import { Axios } from "axios";
+import React from "react";
 const BookIssuePage = () => {
+  const [bookIssu, setBookIssu] = useState([]);
+  const url = "";
+  const token = localStorage.getItem("token");
+  const obj = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    }
+  }
+  useEffect(() =>{
+    const issuBook = async () => {
+      try {
+        const response = await Axios.get(url, obj);
+        const data = await response.data;
+        setBookIssu(data);
+      }
+      catch (error){
+        console.log("Your code be drink beer",error);
+      }
+    }
+    issuBook();
+  }),[];
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <h1 className="text-3xl font-bold">Book Issue</h1>
